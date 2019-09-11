aa79be101c23d88d335982f34572df93cc77b34b
Robert Kulp <pedalinon@gmail.com>
2019-09-10 17:35:19 -0700

    Trying to add to state. Time for a break

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -3,41 +3,62 @@ import { useState } from 'react';
 import './App.css';
 
 const Form = props => {
-	const addTeamMembers = {
-		name: '', 
-		email: '',
-		role: ''
-	}
+	// const addTeamMembers = [
+	// 	{
+	// 		name: '', 
+	// 		email: '',
+	// 		role: ''
+	// 	}
+	// ];
 
-	const [newMember, addNewMember] = useState();
+	const [newMember, addNewMember] = useState([
+			{
+			name: '', 
+			email: '',
+			role: ''
+		}
+	]);
 
 	const handleChange = event => {
+		console.log('Handle Change')
 		addNewMember({
 			...newMember,
 			[event.target.name]: event.target.value
 		});
 	};
 
+	const handleSubmit = event => {
+		event.preventDefault();
+
+		addNewMember([newMember, ...newMember]);
+	}
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<h3>Awesome Team Members!</h3>
 			<input 
 				type='text'
 				name='name'
 				placeholder='FULLNAME'
+				onChange={handleChange}
+				value={newMember.name}
 			/>
 			<input 
 				type='text'
 				name='email'
 				placeholder='EMAIL'
+				onChange={handleChange}
+				value={newMember.email}
 			/>
 			<input
 				type='text'
 				name='role'
 				placeholder='ROLE'
+				onChange={handleChange}
+				value={newMember.role}
 			/>
 
-			<button>ADD YOURSELF NOW</button>
+			<button type='submit'>ADD YOURSELF NOW</button>
 		</form>
 	);
 } 
